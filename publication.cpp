3810733491b4f9672097577483d7552d8c027aa7
Emmanuel Gonzalez <emmanuel.gonzalez@mavs.uta.edu>
2017-09-27 17:27:43 -0500

    edited publication.cpp by removing the std:: next to each string

--- publication.cpp
+++ publication.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <string>
 #include "publication.h"
 using namespace std;
 
-Publication::Publication(std::string title, std::string author, std::string copyright,
-  Genre genre, Media media, Age target_age, std::string isbn) : _title{title},
+Publication::Publication(string title, string author, string copyright,
+  Genre genre, Media media, Age target_age, string isbn) : _title{title},
   _author{author}, _copyright{copyright}, _genre{genre}, _media{media},
   _target_age{target_age}, _isbn{isbn} {}
 
-void Publication::check_out(std::string patron_name, std::string patron_phone) {
+void Publication::check_out(string patron_name, string patron_phone) {
   checked_out = true;
   _patron_name = patron_name;
   _patron_phone = patron_phone;
@@ -21,7 +22,7 @@ bool Publication::is_checked_out() {
   return checked_out;
 }
 
-std::string Publication::to_string() {
+string Publication::to_string() {
   return "\"" + _title + "\" by " + _author + ", " + _copyright + " (" +
           _target_age.to_string(); + " " + _media.to_string(); + " " +
           _genre.to_string(); + ") ISBN: " + _isbn + "\nChecked out to " +
