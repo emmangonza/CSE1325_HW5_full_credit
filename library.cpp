c8b9be83bfc7fbe3819c5f878091b3c840680a8d
Emmanuel Gonzalez <emmanuel.gonzalez@mavs.uta.edu>
2017-09-27 22:45:59 -0500

    added to the add publication method in library.cpp

--- library.cpp
+++ library.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
-#include "library.h"
 #include <vector>
 #include <string>
+#include "library.h"
+#include "publication.h"
 using namespace std;
 
 void Library::add_publication(Publication pub) {
+  string title, author, copyright, genre, media, age, isbn;
+
+  cout << "Enter the title: ";
+  getline(cin, title);
+  cout << "Enter the author: ";
+  getline(cin, author);
+  cout << "Enter the copyright year: ";
+  getline(cin, copyright);
+  cout << "Enter the genre(book, periodical, newspaper, audio, video): ";
+  getline(cin, genre);
+  cout << "Enter the media(fiction, non-fiction, self-help, performance): ";
+  getline(cin, media);
+  cout << "Enter the target age(children, teen, adult, restricted): ";
+  getline(cin, age);
+  cout << "Enter the isbn number: ";
+  getline(cin, isbn);
+
+  Genre pub_genre(genre);
+  Media pub_media(media);
+  Age pub_age(age);
+
+  pub(title, author, copyright, pub_genre, pub_media, pub_age, isbn);
+
   publications.push_back(pub);
 }
 
@@ -17,7 +41,7 @@ void Library::check_in(int publication_index) {
 }
 
 string Library::publication_to_string(int publication_index) {
-  publications[publication_index].to_string();
+  return publications[publication_index].to_string();
 }
 
 int number_of_publications() {
