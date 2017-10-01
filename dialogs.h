f5b3e6ca534663e73e0c6c61a984ba4be39fb51f
Emmanuel Gonzalez <emmanuel.gonzalez@mavs.uta.edu>
2017-10-01 17:06:40 -0500

    added the other 3 functions to the dialogs class

--- dialogs.h
+++ dialogs.h
@@ -8,6 +8,15 @@ using namespace std;
 
 class Dialogs {
   public:
+
     static void message(string msg, string title);
+
+    static int question(string msg, string title = "Question",
+                 vector<string> buttons = {"Cancel", "OK"});
+
+    static string input(string msg, string title = "Input", string default_text = "",
+                 string cancel_text = "CANCEL");
+
+    static void image(string filename, string title = "Image", string msg = "");
 };
 #endif
