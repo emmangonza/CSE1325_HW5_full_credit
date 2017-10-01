b0274d2d5e164c9d929b1c4d1dab55db353edf1f
Emmanuel Gonzalez <emmanuel.gonzalez@mavs.uta.edu>
2017-09-30 21:20:26 -0500

    added exception throws in publication.cpp

--- publication.cpp
+++ publication.cpp
@@ -1,6 +1,8 @@
+#include "publication.h"
+
 #include <iostream>
 #include <string>
-#include "publication.h"
+
 using namespace std;
 
 Publication::Publication(string title, string author, string copyright,
@@ -9,13 +11,23 @@ Publication::Publication(string title, string author, string copyright,
   _target_age{target_age}, _isbn{isbn} {}
 
 void Publication::check_out(string patron_name, string patron_phone) {
-  checked_out = true;
-  _patron_name = patron_name;
-  _patron_phone = patron_phone;
+  if(checked_out == true) {
+    throw Invalid_transaction();
+  }
+  else {
+    checked_out = true;
+    _patron_name = patron_name;
+    _patron_phone = patron_phone;
+  }
 }
 
 void Publication::check_in() {
-  check_out = false;
+  if(check_out == true) {
+    checked_out = false;
+  }
+  else {
+    throw Invalid_transaction();
+  }
 }
 
 bool Publication::is_checked_out() {
@@ -23,8 +35,14 @@ bool Publication::is_checked_out() {
 }
 
 string Publication::to_string() {
+  if(checked_out == true) {
+    return "\"" + _title + "\" by " + _author + ", " + _copyright + " (" +
+            _target_age.to_string(); + " " + _media.to_string(); + " " +
+            _genre.to_string(); + ") ISBN: " + _isbn + "\nChecked out to " +
+            _patron_name + " (" + _patron_phone + ")";
+  }
+
   return "\"" + _title + "\" by " + _author + ", " + _copyright + " (" +
           _target_age.to_string(); + " " + _media.to_string(); + " " +
-          _genre.to_string(); + ") ISBN: " + _isbn + "\nChecked out to " +
-          _patron_name + " (" + _patron_phone + ")";
+          _genre.to_string(); + ") ISBN: " + _isbn + "\nNot checked out";
 }
